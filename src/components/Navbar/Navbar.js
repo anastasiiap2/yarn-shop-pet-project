a27c23ea10d525c6db97fc05732f06af56706b39
anastasiiap2 <anastasiia.pylyp@gmail.com>
2023-04-02 22:59:56 +0200

    Add Yarn dropdown. add mixin rule. dropdown-btn rename to menu-btn. create state "isShown" to toggle yarn dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,9 +5,15 @@ import "./navbar.sass";
 
 export default function Navbar() {
     const [isActive, setActive] = useState(false);
+    const [isShown, setIsShown] = useState(false);
 
     const toggleMenuButton = () => {
         setActive(!isActive)
+        setIsShown(false)
+    }
+
+    const toggleYarnButton = () => {
+        isActive && setIsShown(!isShown)
     }
 
     return <>
@@ -18,29 +24,29 @@ export default function Navbar() {
 
                 </svg>
             </div>
-            <button className="dropdown-btn" onClick={toggleMenuButton}>
+            <button className="menu-btn" onClick={toggleMenuButton}>
                 Menu
             </button>
             <div className={isActive ? "visible flex-item nav-list" : "flex-item nav-list"}>
-                <Link to="/crochet-hooks" className={isActive ? "visible nav-link" : "nav-link"}>
+                <Link to="/crochet-hooks" className={isActive ? "visible nav-link" : "nav-link"} onClick={toggleMenuButton}>
                     Crochet hooks
                 </Link>
-                <Link to="/auxiliary-tools" className={isActive ? "visible nav-link" : "nav-link"}>
+                <Link to="/auxiliary-tools" className={isActive ? "visible nav-link" : "nav-link"} onClick={toggleMenuButton}>
                     Auxiliary tools
                 </Link>
-                <Link to="/knitting-needles" className={isActive ? "visible nav-link" : "nav-link"}>
+                <Link to="/knitting-needles" className={isActive ? "visible nav-link" : "nav-link"} onClick={toggleMenuButton}>
                     Knitting needles
                 </Link>
-                <div className={isActive ? "visible nav-link yarn-btn" : "nav-link yarn-btn"}>
-                    <span>Yarns</span>
-                    <div className="yarn-dropdown">
-                        <Link to="/yarn/acrylic-yarn" className="nav-link">Acrylic yarn</Link>
-                        <Link to="/yarn/mohair-yarn" className="nav-link">Mohair yarn</Link>
-                        <Link to="/yarn/cotton-yarn" className="nav-link">Cotton yarn</Link>
-                        <Link to="/yarn/polyester-yarn" className="nav-link">Polyester yarn</Link>
-                        <Link to="/yarn/alpaca-yarn" className="nav-link">Alpaca yarn</Link>
+                <div className={isActive ? "visible nav-link yarn-div" : "nav-link yarn-div"}>
+                    <button onClick={toggleYarnButton} className={isActive ? "visible yarn-btn nav-link" : "yarn-btn nav-link"}>Yarns</button>
+                    <div className={isShown ? "shown yarn-dropdown" : "yarn-dropdown"}>
+                        <Link to="/yarn/acrylic-yarn" className={isShown ? "yarn-link nav-link" : "nav-link"} onClick={toggleMenuButton}>Acrylic yarn</Link>
+                        <Link to="/yarn/mohair-yarn" className={isShown ? "yarn-link nav-link" : "nav-link"} onClick={toggleMenuButton}>Mohair yarn</Link>
+                        <Link to="/yarn/cotton-yarn" className={isShown ? "yarn-link nav-link" : "nav-link"} onClick={toggleMenuButton}>Cotton yarn</Link>
+                        <Link to="/yarn/polyester-yarn" className={isShown ? "yarn-link nav-link" : "nav-link"} onClick={toggleMenuButton}>Polyester yarn</Link>
+                        <Link to="/yarn/alpaca-yarn" className={isShown ? "yarn-link nav-link" : "nav-link"} onClick={toggleMenuButton}>Alpaca yarn</Link>
                     </div>
-                </div>
+                 </div>
             </div>
         </nav>
     </>
