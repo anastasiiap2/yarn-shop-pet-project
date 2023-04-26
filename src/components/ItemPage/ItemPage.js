93e69eac880ed527e3c6481ab6a6110a0a4468c6
anastasiiap2 <anastasiia.pylyp@gmail.com>
2023-04-26 21:50:42 +0200

    handle radio select + form submit. Not sending data to server yet

--- src/components/ItemPage/ItemPage.js
+++ src/components/ItemPage/ItemPage.js
@@ -6,30 +6,42 @@ import "./itemPage.sass";
 import CartMini from "../Cart/CartMini";
 
 export default function ItemPage() {
-    const [resp, setResp] = useState({});
+    const [item, setItem] = useState({});
     const [imageSource, setImageSource] = useState('');
+    const [form, setForm] = useState({
+        option: '',
+        quantity: 1
+    });
 
     const params = useParams();
-
-    const handleAddItem = (event) => {
-
-    }
-
 
     useEffect(() => {
         fetch(`http://127.0.0.1:3000/items/${params.id}.json`)
             .then(response => response.json())
             .then(data => {
-                setResp(data)
+                setItem(data)
                 setImageSource(data.image0)
-                console.log(resp.sizes)
+                console.log(item.sizes)
             })
     }, [])
 
-    const handleChangeImage = (event) => {
+    const handleImageChange = (event) => {
         let source = event.target.getAttribute('src')
         console.log(source)
         setImageSource(source)
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        console.log(form)
+    }
+
+    const handleRadioSelect = (event) => {
+        setForm({...form, option: event.target.value})
+    }
+
+    const handleQuantitySelect = (event) => {
+        setForm({...form, quantity: event.target.value})
     }
 
     return <>
@@ -40,31 +52,31 @@ export default function ItemPage() {
                     <div>
                         <img src={imageSource} alt="" width="300" height="300"/>
                         <ul className="image-list">
-                            <li className=""><img src={resp.image0} alt="" width="70" height="70"
+                            <li className=""><img src={item.image0} alt="" width="70" height="70"
                                                   className="list-image-item"
-                                                  onClick={handleChangeImage}/></li>
-                            <li className=""><img src={resp.image1} alt="" width="70" height="70"
+                                                  onClick={handleImageChange}/></li>
+                            <li className=""><img src={item.image1} alt="" width="70" height="70"
                                                   className="list-image-item"
-                                                  onClick={handleChangeImage}/></li>
-                            <li className=""><img src={resp.image2} alt="" width="70" height="70"
+                                                  onClick={handleImageChange}/></li>
+                            <li className=""><img src={item.image2} alt="" width="70" height="70"
                                                   className="list-image-item"
-                                                  onClick={handleChangeImage}/></li>
+                                                  onClick={handleImageChange}/></li>
                         </ul>
                     </div>
                 </div>
                 <div className="item-description-container">
-                    <h1 className="display-6">{resp.title}</h1>
+                    <h1 className="display-6">{item.title}</h1>
                     <div className="price-div">
-                        <p id="item-price">{resp.price}zł</p>
+                        <p id="item-price">{item.price}zł</p>
                         <img src={priceTag} alt="" width="30" height="30" id="price-tag"/>
                     </div>
-                    <p id="item-description">{resp.description}</p>
-                    <form action="">
+                    <p id="item-description">{item.description}</p>
+                    <form action="" onSubmit={handleFormSubmit}>
                         <div className="list-items">
-                            {resp.sizes && resp.sizes.map((size, index) => {
+                            {item.sizes && item.sizes.map((size, index) => {
                                 return <li key={index} id="option-list-item">
                                     <label aria-label={size}>
-                                        <input type="radio" id="input-radio" name="option"/>
+                                        <input type="radio" id="input-radio" name="option" onChange={handleRadioSelect} value={size}/>
                                         <span id="option">{size}</span>
                                     </label>
                                 </li>
@@ -74,9 +86,9 @@ export default function ItemPage() {
                             <div className="input-group">
                                 <label htmlFor="quantity" className="input-group-text">Quantity:</label>
                                 <input type="number" className="form-control" pattern="[0-9]*" min="1" step="1"
-                                       id="quantity" name="quantity" aria-label="Input field for quantity"/>
+                                       id="quantity" name="quantity" aria-label="Input field for quantity" onChange={handleQuantitySelect} value={form.quantity}/>
                             </div>
-                            <button id="buy-btn" onClick={handleAddItem} type="submit">Add</button>
+                            <button id="buy-btn" type="submit">Add</button>
                         </div>
                     </form>
                 </div>
